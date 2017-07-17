d1cb4071e0a33648995b70fabb4f6cbcd45324cb
Andrej Tozon <andrej@tozon.info>
2017-07-17 18:21:37 +0200

    MainPage with SplitView navigation prep.

--- CognitiveServicesPlayground/App.xaml.cs
+++ CognitiveServicesPlayground/App.xaml.cs
@@ -56,7 +56,7 @@ namespace CognitiveServicesPlayground
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(FacePage), e.Arguments);
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
